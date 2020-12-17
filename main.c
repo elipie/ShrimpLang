0b5812f009ee48793514bd39def34a11782b1cdc
Eli <72884616+elipie@users.noreply.github.com>
2020-12-17 10:45:40 -0600

    Create main.c & add starter framwork

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char* read_file(char* fname) {
+  FILE* f = fopen(fname, "r");
+  char* buffer = NULL;
+  int string_size, read_size;
+
+  if(f) {
+    fseek(f, 0, SEEK_END);
+    string_size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    buffer = malloc(sizeof(char) * (string_size + 1));
+    buffer[string_size] = '\0';
+
+    read_size = fread(buffer, sizeof(char), string_size, f);
+
+    if(string_size != read_size) {
+      free(buffer);
+      printf("Unable to allocate file memory!\n");
+      exit(1);
+    }
+    
+    fclose(f);
+
+    return buffer;
+  } else {
+    printf("Unable to read file!\n");
+    exit(1);
+  }
+}
+
+int main() {
+  char* filename = "main.shr";
+
+  char* contents = read_file(filename);
+  
+  // todo — everything
+
+  return 0;
+} 
