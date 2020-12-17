e8da4769bf8773f2e7eedf4e6a0d84920ae5f083
Eli <72884616+elipie@users.noreply.github.com>
2020-12-17 10:51:07 -0600

    Added token enums

--- lexer.h
+++ lexer.h
@@ -1,9 +1,23 @@
 #ifndef lexer_h
 #define lexer_h
 struct Token {
-  enum TokenType {
+enum TokenType {
+  T_VAR, // var
+  T_WHILE, // while 
+  T_DO, // do 
+  T_END, // end 
+  T_PRINT, // println
+  T_ID, // x, y, z
+  T_NUM,  // 1, 2, 3
+  T_LPAR, // (
+  T_RPAR, //)
+  T_PLUS, //+
+  T_MINUS, // -
+  T_EQ, //=
+  T_SEP, // force newline
+  T_EOF // end of file
+} type;
 
-  } type;
 
   char* value;
 
